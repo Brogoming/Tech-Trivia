139d83e7290a61d48938e409874b18a93602a7de
Kale <kdwetjen@icloud.com>
2025-02-07 19:28:46 -0600

    Homepage styling + new button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Home</h1>
-<a href="/board/game">Play Game</a>
+<div class="min-h-screen flex flex-col">
+
+    <div class="flex w-1/2 mx-auto p-4">
+        <div class="flex-initial font-bold px-4 py-2 m-2 mx-auto p-4 rounded" >
+            <h1 class="text-8xl">Tech Trivia</h1>
+        </div>
+    </div>
+
+
+    <div class="fixed bottom-10 left-0 right-0 flex justify-center w-full">
+      <button class="w-full md:w-auto relative hover:bg-blue-700 text-white font-bold py-2 px-4">
+        <a href="/board/game" class="text-5xl">Start Game</a>
+      </button>
+    </div>
+
+</div>
+
+  
