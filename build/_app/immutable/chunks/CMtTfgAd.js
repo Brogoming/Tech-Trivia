8456afb6215b201bec74b2ae2d911efc487665a2
Dakota Gullicksen <kotag@outlook.com>
2025-03-23 17:19:00 -0500

    deleted build

--- build/_app/immutable/chunks/CMtTfgAd.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_cj0gk4)==null?void 0:s.base)??"/TechTrivia";var a;const t=((a=globalThis.__sveltekit_cj0gk4)==null?void 0:a.assets)??e;export{t as a,e as b};
