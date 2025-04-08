aaab1503340d9ba8ce7755dd44c9980e73c3a937
RobertDSchwartz <98043586+RobertDSchwartz@users.noreply.github.com>
2025-04-08 17:35:51 -0500

    Default games4 6 (#16)
    * updated game 4
    
    added all categories/questions for game 4
    
    * game 5 complete
    
    added question and answer pairs to game 5
    
    * created game 6
    
    created the questions/answers for game 6 and I also made the formatting consistent between all of the games
    
    * looks good I just swapped the '
    
    ---------
    
    Co-authored-by: Dakota Gullicksen <kotag@outlook.com>

--- src/premadeSets/game3.js
+++ src/premadeSets/game3.js
@@ -1,15 +1,30 @@
 export default {
 	'Myths and Legends': {
 		'What technology-related fear led to widespread panic in 1999 due to concerns that computers would malfunction at the turn of the millennium?':
-			{ answer: 'Y2K Bug', points: 100 },
+			{
+				answer: 'Y2K Bug',
+				points: 100
+			},
 		'Which famous inventor is often mistakenly credited with inventing the light bulb, despite only improving upon existing designs?':
-			{ answer: 'Thomas Edison', points: 200 },
+			{
+				answer: 'Thomas Edison',
+				points: 200
+			},
 		'What is the name of this bronze giant built by Hephaestus with the main purpose of protecting the island of Crete?':
-			{ answer: 'Talos', points: 300 },
+			{
+				answer: 'Talos',
+				points: 300
+			},
 		'In 2000, which company was falsely rumored to be working on an "X-ray vision" feature for its new mobile phone cameras?':
-			{ answer: 'Sony', points: 400 },
+			{
+				answer: 'Sony',
+				points: 400
+			},
 		'Which supposed U.S. Navy experiment allegedly made a warship invisible and teleported to another location in 1943?':
-			{ answer: 'The Philadelphia Experiment', points: 500 }
+			{
+				answer: 'The Philadelphia Experiment',
+				points: 500
+			}
 	},
 	Companies: {
 		'What company first introduced their Chromebook hardware in 2011?': {
@@ -17,27 +32,51 @@ export default {
 			points: 100
 		},
 		'What technology company was founded in 1993 with the vision that the next wave of computing would be graphics-based? The company took its name from the Latin word for "envy" and features product families GeForce, Quadro, and Tegra.':
-			{ answer: 'Nvidia', points: 200 },
+			{
+				answer: 'Nvidia',
+				points: 200
+			},
 		'THINK was the company motto for more than 40 years, for the company often referred to as "Big Blue." What is this frequently-acronymed company?':
-			{ answer: 'IBM', points: 300 },
+			{
+				answer: 'IBM',
+				points: 300
+			},
 		'In 1994, which computer company launched its first web browser, Navigator?': {
 			answer: 'Netscape',
 			points: 400
 		},
 		'The Massachusetts Institute of Technology (MIT) is the majority owner of what manufacturing company which predominantly sells audio equipment? The company was founded by an associate professor who later in life donated his shares to the school.':
-			{ answer: 'Bose', points: 500 }
+			{
+				answer: 'Bose',
+				points: 500
+			}
 	},
 	People: {
 		"Steve Jobs is famously one of the two men who co-founded Apple in 1976. The other co-founder was also named Steve. What was this other man's surname?":
-			{ answer: 'Wozniak', points: 100 },
+			{
+				answer: 'Wozniak',
+				points: 100
+			},
 		'In 2004, \"thefacebook\" was launched as a social networking site for Harvard by students led by which computer programming student who later dropped out to become the face of the site (so to speak)?':
-			{ answer: 'Mark Zuckerberg', points: 200 },
+			{
+				answer: 'Mark Zuckerberg',
+				points: 200
+			},
 		'It wasn\'t until 2016 that Tim Berners-Lee won the Association for Computing Machinery\'s top \"Nobel-level\" award, named for what British mathematician and computer scientist?':
-			{ answer: 'Alan Turing', points: 300 },
+			{
+				answer: 'Alan Turing',
+				points: 300
+			},
 		"Named in Forbes magazine\'s Top 10 Most Powerful Women every year since 2010, which Dallas-born former computer scientist only received about 2% of her former husband\'s roughly $100 billion wealth when they divorced in 2021?":
-			{ answer: 'Melinda Gates', points: 400 },
+			{
+				answer: 'Melinda Gates',
+				points: 400
+			},
 		'Which American computer scientist co-founded Sun Microsystems and played a significant role in the development of the Java programming language?':
-			{ answer: 'James Gosling', points: 500 }
+			{
+				answer: 'James Gosling',
+				points: 500
+			}
 	},
 	'Fictional Shows': {
 		'Which animated TV show features a character named Bender, who is a bending unit robot?': {
@@ -45,13 +84,25 @@ export default {
 			points: 100
 		},
 		'Which British TV series features a time-traveling alien known as the Doctor, who explores the universe in a ship called the TARDIS?':
-			{ answer: 'Doctor Who', points: 200 },
+			{
+				answer: 'Doctor Who',
+				points: 200
+			},
 		'Played by LeVar Burton, Lieutenant Geordi La Forge wore a VISOR, a curved device worn over the face like sunglasses, to restore his vision on which TV series in the "Star Trek" franchise?':
-			{ answer: 'The Next Generation', points: 300 },
+			{
+				answer: 'The Next Generation',
+				points: 300
+			},
 		'Which TV series is set in a futuristic theme park populated by android "hosts" where guests can live out their fantasies?':
-			{ answer: 'Westworld', points: 400 },
+			{
+				answer: 'Westworld',
+				points: 400
+			},
 		'Which 1980s TV show featured a sentient car named KITT, equipped with advanced artificial intelligence?':
-			{ answer: 'Knight Rider', points: 500 }
+			{
+				answer: 'Knight Rider',
+				points: 500
+			}
 	},
 	'Other Sciences': {
 		'Who is credited with inventing the first successful airplane?': {
@@ -65,7 +116,10 @@ export default {
 			points: 300
 		},
 		'Which physicist developed the theory of electromagnetism and has a set of equations named after him?':
-			{ answer: 'James Clark Maxell', points: 400 },
+			{
+				answer: 'James Clark Maxell',
+				points: 400
+			},
 		'If C in the words CT scan stands for Computed, what does the T stand for?': {
 			answer: 'Tomography',
 			points: 500
@@ -81,7 +135,10 @@ export default {
 			points: 200
 		},
 		'What is the name of the humanoid robot developed by Boston Dynamics, known for its advanced mobility and agility?':
-			{ answer: 'Atlas', points: 300 },
+			{
+				answer: 'Atlas',
+				points: 300
+			},
 		'In which year was the term "robot" first introduced in Karel Čapek\'s play "R.U.R."?': {
 			answer: '1921',
 			points: 400
